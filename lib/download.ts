eb851282fce550db41eef9ff4aae2685c611fa24
YuJian920 <HAO52013141@Gmail.com>
2023-04-03 14:58:53 +0800

    修改文件名拓展名获取方式

--- lib/download.ts
+++ lib/download.ts
@@ -1,14 +1,14 @@
 import filenamify from "filenamify";
 import { ensureDir } from "fs-extra";
 import download from "nodejs-file-downloader";
-import path from "path";
+import { resolve, extname } from "node:path";
 import { downloadDir } from "../config/config.json";
 import { SpiderQueue } from "../type";
 import progressBar from "../utils/progressBar";
 
 export const saveVideo = async (videoQueue: SpiderQueue[], dir: string) => {
   console.log("开始下载 ===>", dir);
-  const targetDir = path.resolve(process.cwd(), downloadDir, filenamify(dir));
+  const targetDir = resolve(process.cwd(), downloadDir, filenamify(dir));
   let _downloadCount = 0;
 
   try {
@@ -16,16 +16,19 @@ export const saveVideo = async (videoQueue: SpiderQueue[], dir: string) => {
     for (const item of videoQueue) {
       try {
         console.log("正在下载 ===>", ++_downloadCount, "项", _downloadCount === 1 ? "" : "\n");
-        const targetFileName = `${item.id}-${filenamify(item.desc)}.mp4`;
         let progress = null
         let downloadHelper = new download({
           url: item.url,
           directory: targetDir,
-          fileName: targetFileName,
           skipExistingFileName: true,
           onProgress: (percentage) => {
             progress = new progressBar('下载进度', 50);
             progress.render({ completed: percentage, total: 100 });
+          },
+          onBeforeSave: (deducedName) => {
+            const fileExt = extname(deducedName)
+            if (fileExt) return `${item.id}-${filenamify(item.desc)}${fileExt}`
+            return deducedName
           },
         });
 
