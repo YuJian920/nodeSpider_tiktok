2583260735a2de09bccdd417037bbe0bae056456
YuJian920 <HAO52013141@Gmail.com>
2023-04-27 09:53:49 +0800

    替换 1080P 下载 URL 为无水印 URL，感谢 @zhangcunxiang

--- utils/config.ts
+++ utils/config.ts
@@ -1,7 +1,7 @@
 export const postBaseUrl = "https://www.douyin.com/aweme/v1/web/aweme/post/?";
 export const likeBaseUrl = "https://www.douyin.com/aweme/v1/web/aweme/favorite/?";
 
-export const HDDownloadUrl = "https://aweme.snssdk.com/aweme/v1/playwm/?";
+export const HDDownloadUrl = "https://aweme.snssdk.com/aweme/v1/play/?";
 
 export const headerOption: HeadersInit = {
   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
